5689f9bd073f9fd68c56b53fab5edd3302113ca4
MichaelCrenshaw <57016022+MichaelCrenshaw@users.noreply.github.com>
2022-07-02 20:46:55 -0600

    Add wordlist

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use std::env;
 use std::io::{stdin, stdout, Write};
+use memorable_wordlist::{space_delimited, WORDS};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
     // todo: parse value from args and feed into gen_word
-    let word: String = gen_word(1);
+    let words: Vec<String> = gen_word(1);
+    println!("{}", word)
 }
 
-fn gen_word(word_count: u32) -> String {
-    return String("hi")
+fn gen_word(word_count: u32) -> Vec<String> {
+    words: Vec<String>;
+    for x in 1..word_count {
+
+    }
+    return words
 }
 
 fn guess_letter(guess: char, answer: String, guesses: Vec<String>) {
