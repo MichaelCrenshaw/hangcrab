37c9bf7a4418f1878e4bca90635537da90e0316e
MichaelCrenshaw <57016022+MichaelCrenshaw@users.noreply.github.com>
2022-07-19 20:26:17 -0600

    Refactored function names to be more descriptive, refactored code to be more readable.

--- src/main.rs
+++ src/main.rs
@@ -3,57 +3,79 @@ use std::error::Error;
 use std::{env, io};
 use std::fs::File;
 use std::process;
+use rand::{random, Rng};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // todo: declare all paths based on file rather than working directory for safety
-    let file_path = "wordlist.csv";
+    // todo: declare all paths based on file rather than working directory
+    let passphrase_file_path = "wordlist.csv";
 
-    let words = get_words(file_path);
-    let words = match words {
-        Ok(words) => words,
+    let phrase_list = match get_phrase_list(passphrase_file_path) {
+        Ok(phrases) => phrases,
         Err(error) => {
             print!("{error}");
-            vec![String::from("taco"), String::from("additional words")]
+            vec![String::from("Artie's"), String::from("We have the beets")]
         }
     };
-    // testcode
-    for line in words{
-        print!("{line}");
-    }
 
-    // todo: parse value from args and feed into gen_word
-    let word = gen_word();
-    println!("{}", word)
+    let mut new_game = true;
+    while new_game{
+        // todo: parse value from args and feed into gen_word
+        let word = match get_random_phrase(&phrase_list) {
+            Some(phrase) => phrase,
+            None => {
+                // todo: prompt for new filepath instead
+                panic!()
+            }
+        };
+        let mut winner = false;
+        while !winner {
+        //     let mut guess: String;
+        //     // todo: handle errors gracefully
+        //     io::stdin().read_line(&mut guess).expect("Failed to read line properly");
+        //
+        //     let chars: Vec<char> = guess.chars().collect();
+        //
+        //     if guess.len() == 1 {
+        //
+        //     }
+        //
+        //
+        }
+        print!("Your phrase is: {word}")
+    }
+    println!("Thanks for playing hangcrab");
+    process::exit(0);
 }
 
-fn get_words(file_path: &str) -> Result<Vec<String>, Box<dyn Error>>{
+fn get_phrase_list(file_path: &str) -> Result<Vec<String>, Box<dyn Error>>{
     let file = File::open(file_path)?;
     let mut reader = csv::ReaderBuilder::new()
         .from_reader(file);
 
-    let mut words = Vec::new();
+    let mut phrases = Vec::new();
     for result in reader.records() {
         for row in result.iter() {
-            let mut row = row.as_slice()?;
-            words.push(String::from(row));
+            let row = row.as_slice();
+            phrases.push(String::from(row));
         }
     }
-    Ok(words)
+    Ok(phrases)
 }
 
-fn gen_word() -> String {
-    let words: String = String::new();
-
-    return words
+fn get_random_phrase(word_list: &Vec<String>) -> Option<String> {
+    let random_int = rand::thread_rng().gen_range(0..word_list.len());
+    let phrase = word_list.get(random_int)?;
+    Some(phrase.to_owned())
 }
 
-fn guess_letter(guess: char, answer: String, guesses: Vec<String>) {
+// todo: disable guessing the same letter twice
+fn guess_letter(guess: &char, answer: &String) {
     return
 }
 
-fn guess_word(guess: String, answer: String, guesses: Vec<String>) {
+fn guess_word(guess: &String, answer: &String) {
     return
 }
 
@@ -61,4 +83,8 @@ fn guess_word(guess: String, answer: String, guesses: Vec<String>) {
 // Add a part of the crab to noose
 fn hang_crustacean_part() {
     return
+}
+
+struct HangcrabPhrase {
+
 }
